988c5ca52a5b918aa2b7009325fc6be162506e68
ING. JEMAI M.Dhia <dhia.jemai@gmail.com>
2019-11-27 19:17:06 +0100

    Test DataBase

--- src/main/java/com/example/demo/Etudiant.java
+++ src/main/java/com/example/demo/Etudiant.java
@@ -4,54 +4,119 @@ package com.example.demo;
 public class Etudiant {
 	
 	private int idEtudiant;
-	private double Matiere1;
-	private double Matiere2;
-	private double Matiere3;
-	private double Matiere4;
-	private double Matiere5;
-	private double Matiere6;
-	private double Matiere7;
-	private double Matiere8;
-	private double Matiere9;
-	private double Matiere10;
-	private double Matiere11;
-	private double Matiere12;
-	private double Matiere13;
-	private double Matiere14;
-	private double Matiere15;
-	private double Matiere16;
-	private double Matiere17;
-	private double Matiere18;
-	private double Matiere19;
-	private double Matiere20;
+	private double ArchitecturedesOrdinateurs;
+	private double TechniquesdeCompilation;
+	private double RechercheOpérationnelle;
+	private double DéveloppementMobile;
+	private double ProgrammationMultimédias;
+	private double Statistiques;
+	private double BasesdeDonnéesAvancées;
+	private double RéseauxInformatiques;
+	private double ConceptionOrientéeObjet;
+	private double ProgrammationOrientéeObjet;
+	private double ConceptiondesBasesdeDonnées;
+	private double SystèmeLogique;
+	private double Probabilités;
+	private double AlgorithmiqueetStructuresdeDonnées;
+	private double Blockchain;
+	private double ArchitectureLogicielle;
+	private double SystèmesExploitation;
+	private double Entrepreneuriat;
+	private double AnalyseNumérique;
+	private double MathPouringénieur;
+	
+	/*****OPTION*******/
+	
+	private double Opt2FouilleDonnées;
+	private double Opt9BigData;
+	private double Opt10DeveloppementiOS;
+	private double Opt3MachineLearning;
+	private double Opt6BusinessIntelligence;
+	private double Opt4TransactionsElectroniques;
+	private double Opt1CloudComputing;
+	private double Opt8SécuritéInformatique;
+	private double Opt5RéseauxIP;
+	private double Opt16RéseauxGSM;
+	private double Opt12ElectroniqueEmbarquée;
+	private double Opt13SystèmeRobotique;
+	private double Opt14ConceptionCartesElectroniques;
+	private double Opt15RéseauCapteurs;
+	private double Opt7QualitéTest;
 	
 	
-	public Etudiant(int idEtudiant, double matiere1, double matiere2, double matiere3, double matiere4,
-			double matiere5, double matiere6, double matiere7, double matiere8, double matiere9, double matiere10,
-			double matiere11, double matiere12, double matiere13, double matiere14, double matiere15, double matiere16,
-			double matiere17, double matiere18, double matiere19, double matiere20) {
+	public Etudiant(int idEtudiant, 
+			double architecturedesOrdinateurs, 
+			double techniquesdeCompilation,
+			double rechercheOpérationnelle, 
+			double développementMobile, 
+			double programmationMultimédias,
+			double statistiques, 
+			double basesdeDonnéesAvancées, 
+			double réseauxInformatiques,
+			double conceptionOrientéeObjet, 
+			double programmationOrientéeObjet, 
+			double conceptiondesBasesdeDonnées,
+			double systèmeLogique, 
+			double probabilités, 
+			double algorithmiqueetStructuresdeDonnées, 
+			double blockchain,
+			double architectureLogicielle, 
+			double systèmesExploitation, 
+			double entrepreneuriat, 
+			double analyseNumérique,
+			double mathPouringénieur, 
+			double opt2FouilleDonnées, 
+			double opt9BigData, 
+			double opt10DeveloppementiOS,
+			double opt3MachineLearning, 
+			double opt6BusinessIntelligence, 
+			double opt4TransactionsElectroniques,
+			double opt1CloudComputing, 
+			double opt8SécuritéInformatique, 
+			double opt5RéseauxIP, 
+			double opt16RéseauxGSM,
+			double opt12ElectroniqueEmbarquée, 
+			double opt13SystèmeRobotique, 
+			double opt14ConceptionCartesElectroniques,
+			double opt15RéseauCapteurs, 
+			double opt7QualitéTest) {
 		super();
 		this.idEtudiant = idEtudiant;
-		Matiere1 = matiere1;
-		Matiere2 = matiere2;
-		Matiere3 = matiere3;
-		Matiere4 = matiere4;
-		Matiere5 = matiere5;
-		Matiere6 = matiere6;
-		Matiere7 = matiere7;
-		Matiere8 = matiere8;
-		Matiere9 = matiere9;
-		Matiere10 = matiere10;
-		Matiere11 = matiere11;
-		Matiere12 = matiere12;
-		Matiere13 = matiere13;
-		Matiere14 = matiere14;
-		Matiere15 = matiere15;
-		Matiere16 = matiere16;
-		Matiere17 = matiere17;
-		Matiere18 = matiere18;
-		Matiere19 = matiere19;
-		Matiere20 = matiere20;
+		ArchitecturedesOrdinateurs = architecturedesOrdinateurs;
+		TechniquesdeCompilation = techniquesdeCompilation;
+		RechercheOpérationnelle = rechercheOpérationnelle;
+		DéveloppementMobile = développementMobile;
+		ProgrammationMultimédias = programmationMultimédias;
+		Statistiques = statistiques;
+		BasesdeDonnéesAvancées = basesdeDonnéesAvancées;
+		RéseauxInformatiques = réseauxInformatiques;
+		ConceptionOrientéeObjet = conceptionOrientéeObjet;
+		ProgrammationOrientéeObjet = programmationOrientéeObjet;
+		ConceptiondesBasesdeDonnées = conceptiondesBasesdeDonnées;
+		SystèmeLogique = systèmeLogique;
+		Probabilités = probabilités;
+		AlgorithmiqueetStructuresdeDonnées = algorithmiqueetStructuresdeDonnées;
+		Blockchain = blockchain;
+		ArchitectureLogicielle = architectureLogicielle;
+		SystèmesExploitation = systèmesExploitation;
+		Entrepreneuriat = entrepreneuriat;
+		AnalyseNumérique = analyseNumérique;
+		MathPouringénieur = mathPouringénieur;
+		Opt2FouilleDonnées = opt2FouilleDonnées;
+		Opt9BigData = opt9BigData;
+		Opt10DeveloppementiOS = opt10DeveloppementiOS;
+		Opt3MachineLearning = opt3MachineLearning;
+		Opt6BusinessIntelligence = opt6BusinessIntelligence;
+		Opt4TransactionsElectroniques = opt4TransactionsElectroniques;
+		Opt1CloudComputing = opt1CloudComputing;
+		Opt8SécuritéInformatique = opt8SécuritéInformatique;
+		Opt5RéseauxIP = opt5RéseauxIP;
+		Opt16RéseauxGSM = opt16RéseauxGSM;
+		Opt12ElectroniqueEmbarquée = opt12ElectroniqueEmbarquée;
+		Opt13SystèmeRobotique = opt13SystèmeRobotique;
+		Opt14ConceptionCartesElectroniques = opt14ConceptionCartesElectroniques;
+		Opt15RéseauCapteurs = opt15RéseauCapteurs;
+		Opt7QualitéTest = opt7QualitéTest;
 	}
 	public int getIdEtudiant() {
 		return idEtudiant;
@@ -59,126 +124,221 @@ public class Etudiant {
 	public void setIdEtudiant(int idEtudiant) {
 		this.idEtudiant = idEtudiant;
 	}
-	public double getMatiere1() {
-		return Matiere1;
+	public double getArchitecturedesOrdinateurs() {
+		return ArchitecturedesOrdinateurs;
 	}
-	public void setMatiere1(double matiere1) {
-		Matiere1 = matiere1;
+	public void setArchitecturedesOrdinateurs(double architecturedesOrdinateurs) {
+		ArchitecturedesOrdinateurs = architecturedesOrdinateurs;
 	}
-	public double getMatiere2() {
-		return Matiere2;
+	public double getTechniquesdeCompilation() {
+		return TechniquesdeCompilation;
 	}
-	public void setMatiere2(double matiere2) {
-		Matiere2 = matiere2;
+	public void setTechniquesdeCompilation(double techniquesdeCompilation) {
+		TechniquesdeCompilation = techniquesdeCompilation;
 	}
-	public double getMatiere3() {
-		return Matiere3;
+	public double getRechercheOpérationnelle() {
+		return RechercheOpérationnelle;
 	}
-	public void setMatiere3(double matiere3) {
-		Matiere3 = matiere3;
+	public void setRechercheOpérationnelle(double rechercheOpérationnelle) {
+		RechercheOpérationnelle = rechercheOpérationnelle;
 	}
-	public double getMatiere4() {
-		return Matiere4;
+	public double getDéveloppementMobile() {
+		return DéveloppementMobile;
 	}
-	public void setMatiere4(double matiere4) {
-		Matiere4 = matiere4;
+	public void setDéveloppementMobile(double développementMobile) {
+		DéveloppementMobile = développementMobile;
 	}
-	public double getMatiere5() {
-		return Matiere5;
+	public double getProgrammationMultimédias() {
+		return ProgrammationMultimédias;
 	}
-	public void setMatiere5(double matiere5) {
-		Matiere5 = matiere5;
+	public void setProgrammationMultimédias(double programmationMultimédias) {
+		ProgrammationMultimédias = programmationMultimédias;
 	}
-	public double getMatiere6() {
-		return Matiere6;
+	public double getStatistiques() {
+		return Statistiques;
 	}
-	public void setMatiere6(double matiere6) {
-		Matiere6 = matiere6;
+	public void setStatistiques(double statistiques) {
+		Statistiques = statistiques;
 	}
-	public double getMatiere7() {
-		return Matiere7;
+	public double getBasesdeDonnéesAvancées() {
+		return BasesdeDonnéesAvancées;
 	}
-	public void setMatiere7(double matiere7) {
-		Matiere7 = matiere7;
+	public void setBasesdeDonnéesAvancées(double basesdeDonnéesAvancées) {
+		BasesdeDonnéesAvancées = basesdeDonnéesAvancées;
 	}
-	public double getMatiere8() {
-		return Matiere8;
+	public double getRéseauxInformatiques() {
+		return RéseauxInformatiques;
 	}
-	public void setMatiere8(double matiere8) {
-		Matiere8 = matiere8;
+	public void setRéseauxInformatiques(double réseauxInformatiques) {
+		RéseauxInformatiques = réseauxInformatiques;
 	}
-	public double getMatiere9() {
-		return Matiere9;
+	public double getConceptionOrientéeObjet() {
+		return ConceptionOrientéeObjet;
 	}
-	public void setMatiere9(double matiere9) {
-		Matiere9 = matiere9;
+	public void setConceptionOrientéeObjet(double conceptionOrientéeObjet) {
+		ConceptionOrientéeObjet = conceptionOrientéeObjet;
 	}
-	public double getMatiere10() {
-		return Matiere10;
+	public double getProgrammationOrientéeObjet() {
+		return ProgrammationOrientéeObjet;
 	}
-	public void setMatiere10(double matiere10) {
-		Matiere10 = matiere10;
+	public void setProgrammationOrientéeObjet(double programmationOrientéeObjet) {
+		ProgrammationOrientéeObjet = programmationOrientéeObjet;
 	}
-	public double getMatiere11() {
-		return Matiere11;
+	public double getConceptiondesBasesdeDonnées() {
+		return ConceptiondesBasesdeDonnées;
 	}
-	public void setMatiere11(double matiere11) {
-		Matiere11 = matiere11;
+	public void setConceptiondesBasesdeDonnées(double conceptiondesBasesdeDonnées) {
+		ConceptiondesBasesdeDonnées = conceptiondesBasesdeDonnées;
 	}
-	public double getMatiere12() {
-		return Matiere12;
+	public double getSystèmeLogique() {
+		return SystèmeLogique;
 	}
-	public void setMatiere12(double matiere12) {
-		Matiere12 = matiere12;
+	public void setSystèmeLogique(double systèmeLogique) {
+		SystèmeLogique = systèmeLogique;
 	}
-	public double getMatiere13() {
-		return Matiere13;
+	public double getProbabilités() {
+		return Probabilités;
 	}
-	public void setMatiere13(double matiere13) {
-		Matiere13 = matiere13;
+	public void setProbabilités(double probabilités) {
+		Probabilités = probabilités;
 	}
-	public double getMatiere14() {
-		return Matiere14;
+	public double getAlgorithmiqueetStructuresdeDonnées() {
+		return AlgorithmiqueetStructuresdeDonnées;
 	}
-	public void setMatiere14(double matiere14) {
-		Matiere14 = matiere14;
+	public void setAlgorithmiqueetStructuresdeDonnées(double algorithmiqueetStructuresdeDonnées) {
+		AlgorithmiqueetStructuresdeDonnées = algorithmiqueetStructuresdeDonnées;
 	}
-	public double getMatiere15() {
-		return Matiere15;
+	public double getBlockchain() {
+		return Blockchain;
 	}
-	public void setMatiere15(double matiere15) {
-		Matiere15 = matiere15;
+	public void setBlockchain(double blockchain) {
+		Blockchain = blockchain;
 	}
-	public double getMatiere16() {
-		return Matiere16;
+	public double getArchitectureLogicielle() {
+		return ArchitectureLogicielle;
 	}
-	public void setMatiere16(double matiere16) {
-		Matiere16 = matiere16;
+	public void setArchitectureLogicielle(double architectureLogicielle) {
+		ArchitectureLogicielle = architectureLogicielle;
 	}
-	public double getMatiere17() {
-		return Matiere17;
+	public double getSystèmesExploitation() {
+		return SystèmesExploitation;
 	}
-	public void setMatiere17(double matiere17) {
-		Matiere17 = matiere17;
+	public void setSystèmesExploitation(double systèmesExploitation) {
+		SystèmesExploitation = systèmesExploitation;
 	}
-	public double getMatiere18() {
-		return Matiere18;
+	public double getEntrepreneuriat() {
+		return Entrepreneuriat;
 	}
-	public void setMatiere18(double matiere18) {
-		Matiere18 = matiere18;
+	public void setEntrepreneuriat(double entrepreneuriat) {
+		Entrepreneuriat = entrepreneuriat;
 	}
-	public double getMatiere19() {
-		return Matiere19;
+	public double getAnalyseNumérique() {
+		return AnalyseNumérique;
 	}
-	public void setMatiere19(double matiere19) {
-		Matiere19 = matiere19;
+	public void setAnalyseNumérique(double analyseNumérique) {
+		AnalyseNumérique = analyseNumérique;
 	}
-	public double getMatiere20() {
-		return Matiere20;
+	public double getMathPouringénieur() {
+		return MathPouringénieur;
 	}
-	public void setMatiere20(double matiere20) {
-		Matiere20 = matiere20;
+	public void setMathPouringénieur(double mathPouringénieur) {
+		MathPouringénieur = mathPouringénieur;
 	}
+	public double getOpt2FouilleDonnées() {
+		return Opt2FouilleDonnées;
+	}
+	public void setOpt2FouilleDonnées(double opt2FouilleDonnées) {
+		Opt2FouilleDonnées = opt2FouilleDonnées;
+	}
+	public double getOpt9BigData() {
+		return Opt9BigData;
+	}
+	public void setOpt9BigData(double opt9BigData) {
+		Opt9BigData = opt9BigData;
+	}
+	public double getOpt10DeveloppementiOS() {
+		return Opt10DeveloppementiOS;
+	}
+	public void setOpt10DeveloppementiOS(double opt10DeveloppementiOS) {
+		Opt10DeveloppementiOS = opt10DeveloppementiOS;
+	}
+	public double getOpt3MachineLearning() {
+		return Opt3MachineLearning;
+	}
+	public void setOpt3MachineLearning(double opt3MachineLearning) {
+		Opt3MachineLearning = opt3MachineLearning;
+	}
+	public double getOpt6BusinessIntelligence() {
+		return Opt6BusinessIntelligence;
+	}
+	public void setOpt6BusinessIntelligence(double opt6BusinessIntelligence) {
+		Opt6BusinessIntelligence = opt6BusinessIntelligence;
+	}
+	public double getOpt4TransactionsElectroniques() {
+		return Opt4TransactionsElectroniques;
+	}
+	public void setOpt4TransactionsElectroniques(double opt4TransactionsElectroniques) {
+		Opt4TransactionsElectroniques = opt4TransactionsElectroniques;
+	}
+	public double getOpt1CloudComputing() {
+		return Opt1CloudComputing;
+	}
+	public void setOpt1CloudComputing(double opt1CloudComputing) {
+		Opt1CloudComputing = opt1CloudComputing;
+	}
+	public double getOpt8SécuritéInformatique() {
+		return Opt8SécuritéInformatique;
+	}
+	public void setOpt8SécuritéInformatique(double opt8SécuritéInformatique) {
+		Opt8SécuritéInformatique = opt8SécuritéInformatique;
+	}
+	public double getOpt5RéseauxIP() {
+		return Opt5RéseauxIP;
+	}
+	public void setOpt5RéseauxIP(double opt5RéseauxIP) {
+		Opt5RéseauxIP = opt5RéseauxIP;
+	}
+	public double getOpt16RéseauxGSM() {
+		return Opt16RéseauxGSM;
+	}
+	public void setOpt16RéseauxGSM(double opt16RéseauxGSM) {
+		Opt16RéseauxGSM = opt16RéseauxGSM;
+	}
+	public double getOpt12ElectroniqueEmbarquée() {
+		return Opt12ElectroniqueEmbarquée;
+	}
+	public void setOpt12ElectroniqueEmbarquée(double opt12ElectroniqueEmbarquée) {
+		Opt12ElectroniqueEmbarquée = opt12ElectroniqueEmbarquée;
+	}
+	public double getOpt13SystèmeRobotique() {
+		return Opt13SystèmeRobotique;
+	}
+	public void setOpt13SystèmeRobotique(double opt13SystèmeRobotique) {
+		Opt13SystèmeRobotique = opt13SystèmeRobotique;
+	}
+	public double getOpt14ConceptionCartesElectroniques() {
+		return Opt14ConceptionCartesElectroniques;
+	}
+	public void setOpt14ConceptionCartesElectroniques(double opt14ConceptionCartesElectroniques) {
+		Opt14ConceptionCartesElectroniques = opt14ConceptionCartesElectroniques;
+	}
+	public double getOpt15RéseauCapteurs() {
+		return Opt15RéseauCapteurs;
+	}
+	public void setOpt15RéseauCapteurs(double opt15RéseauCapteurs) {
+		Opt15RéseauCapteurs = opt15RéseauCapteurs;
+	}
+	public double getOpt7QualitéTest() {
+		return Opt7QualitéTest;
+	}
+	public void setOpt7QualitéTest(double opt7QualitéTest) {
+		Opt7QualitéTest = opt7QualitéTest;
+	}
+	
+	
+	
+	
+	
 	
 	
 	
